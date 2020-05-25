5e38ea26b1b226587a77657904d0fcd76c0db0d1
m4ddabs <43279651+m4ddabs@users.noreply.github.com>
2020-05-25 10:48:13 -0400

    Added comments to all files to explain the program

--- fonctions.c
+++ fonctions.c
@@ -7,27 +7,28 @@
 Alerte* Verification(Position pos, Alerte* alerte,int* taille_tab_alerte,int nmbdeformlocales)
 {
     int i;
-    for (i=0;i<(*taille_tab_alerte);i++)
+    for (i=0;i<(*taille_tab_alerte);i++) //Boucle pour chercher si l'alerte est deja dans le tableau
     {
         if (pos==(alerte+i)->position)
         {
-            return NULL;
+            return NULL;                  //Si elle existe on retourne NULL
         }
     }
-    Alerte* newalerte;
+    Alerte* newalerte                      //Creation de un nouveau pointeur qui va contenir l'adresse a retourner
     *taille_tab_alerte=*taille_tab_alerte+1;
-    newalerte = realloc(alerte,sizeof(Alerte)*(*taille_tab_alerte));
-    if(newalerte == NULL)
+    newalerte = realloc(alerte,sizeof(Alerte)*(*taille_tab_alerte));//la fonction realloc va retourner un nouveau tableau avec la nouvelle alerte
+    if(newalerte == NULL)                                           //Dans le cas ou realloc ne marche pas elle retourne NULL donc ce if sert a gerer cette situation
     {
        printf("un probleme est survenu lors de l'allocation de memoire\n");
        return NULL;
     }
-    (newalerte+i)->position=pos;
+    (newalerte+i)->position=pos;                                     // Si on ne rentre pas dans le if le programme continue normalement et ajoute les info de la nouvelle alerte dans le tableau
     (newalerte+i)->nombre_deformations_locales = nmbdeformlocales;
     return newalerte;
 }
 
 Alerte* ChercheAlerte(Position* paquet,int taillepak){
+   /** Inittialisation des variable **/
    Alerte* alertes = NULL;
    Alerte* newalertes;
    int i = 0;
@@ -36,14 +37,19 @@ Alerte* ChercheAlerte(Position* paquet,int taillepak){
    int premierealerte = 0;
    int premierepos;
    int taillealertes = 0;
+
+   /** Premiere boucle **/
+   /** Elle serte a chercher la premiere alerte. Si il n'y a pas d'alerte
+        alertes reste initialise sur NULL donc le programme se terminera **/
+
    while(premierealerte == 0 && i!=taillepak){
       for(k=0;k<taillepak;k++){
-         if(abs(paquet[i]-paquet[k])<=100){
-            nmbdeformlocales = nmbdeformlocales + 1;
+         if(abs(paquet[i]-paquet[k])<=100){          //Ici nous cherchon la distance entre une position donnee et totues les autres positions du paquet
+            nmbdeformlocales = nmbdeformlocales + 1; //Si cette distance <100 ou = 100 donc on la compte pour former une alerte
          }
       }
-      if(nmbdeformlocales>=100){
-         alertes = malloc(sizeof(Alerte));
+      if(nmbdeformlocales>=100){            //On est encore sur une position donnee, si le nomvre de deformations locales > 100 ou = 100
+         alertes = malloc(sizeof(Alerte));  //Nous allons creer une nouvelle alerte et sortir de la premiere boucle
          taillealertes = taillealertes + 1;
          alertes[0].position = paquet[i];
          alertes[0].nombre_deformations_locales = nmbdeformlocales;
@@ -51,34 +57,40 @@ Alerte* ChercheAlerte(Position* paquet,int taillepak){
          premierepos = i+1;
          premierealerte = 1;
       }
-      else{
+      else{                                 //Sinon nous continuerons jusqu'a trouver un alerte
          nmbdeformlocales=0;
          i++;
       }
    }
-   if(alertes == NULL){
+   if(alertes == NULL){                    // Si il n'y a pas eu d'alertes nous arrettons le programme
       printf("pas d'alertes\n");
       return alertes;
    }
-   for(i=premierepos;i<taillepak;i++){
+
+   /**Deuxieme boucle**/
+   /** Elle sert a chercher le reste des alertes
+   Elle presque identique a la premiere boucle sauf que ici nous n'arreterons pas si on trouve une alerte et nous allons verfier
+   si une alerte donnee existe deja dans le tableau d'alertes avant de la rentrer dans celui ci**/
+
+   for(i=premierepos;i<taillepak;i++){ // On parcours le paquet
       for(k=0;k<taillepak;k++){
          if(abs(paquet[i]-paquet[k])<=100){
             nmbdeformlocales = nmbdeformlocales + 1;
          }
       }
       if(nmbdeformlocales>=100){
-         newalertes = Verification(paquet[i],alertes,&taillealertes,nmbdeformlocales);
-         if(newalertes != NULL){
+         newalertes = Verification(paquet[i],alertes,&taillealertes,nmbdeformlocales); // On fait appel a Verification
+         if(newalertes != NULL){ //Si nous avons un nouveau tableau nous passons l'adresse a alertes et on repointe newalertes a NULL
             alertes = newalertes;
             newalertes = NULL;
          }
-         nmbdeformlocales = 0;
+         nmbdeformlocales = 0; //Sinon newalertes reste sur NULL
       }
       else{
       nmbdeformlocales = 0;
       }
 
-   }
+   }                                               //on retourne l'adresse d'alertes et on donne le nombre d'alertes
    printf("nombre d alertes : %d\n", taillealertes);
    return alertes;
 }
--- fonctions.h
+++ fonctions.h
@@ -21,7 +21,18 @@ typedef struct Alerte Alerte;
 
 //Fonctions Pour chercher alertes
 
+
+/**La fonction ChercheAlerte prends comme entree un pointeur sur un paquet et la taille de ce paquet.
+Elle sert a chercher les nombre d'alertes et retourne un pointeur a un tableau qui contient toutes les alertes. Si il n'y a pas
+d'alertes elle retourne un pointeur sur NULL **/
 Alerte* ChercheAlerte(Position* paquet,int taillepak);
+
+/**La fonction Verification est utilisee dans la fonction ChercheAlerte, elle sert a verifier si une alerte existe deja dans le
+tableau d'alertes, si elle existe elle n'ajoutera rien au tableau et si elle n'existe pas elle ajoutera la nouvelle alerte au tableau.
+Cette fonction prends en entree la position de l'alerte a verifier, un pointeur sur un tableau d'alertes, un pointeur sur la taille
+du tableau d'alertes et le nombre deformation locales dans cette position a verifier.
+La fonction retourne un pointeur sur le tableau contenant la nouvelle alerte si celle ci n'existait pas avant sinon elle retourne un pointeur
+ sur NULL**/
 
 Alerte* Verification(Position pos, Alerte* alerte,int* taille_tab_alerte,int nmbdeformlocales);
 
